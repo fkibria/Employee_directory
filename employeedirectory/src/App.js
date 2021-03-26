624c75d8b17109de5a083198991ceb2df30cebd7
fkibria <fkibria02@gmail.com>
2021-03-26 13:06:04 -0400

    fixed error, added local instance of state in first 2 sort methods

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -10,75 +10,90 @@ class App extends Component {
     ascend: true
   };
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://randomuser.me/api/?results=25&nat=ca')
-    .then((response) =>{
-      console.log(response)
-      this.setState({
-        employees: response.data.results
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          employees: response.data.results
+        })
       })
-    })
   }
-  
+
   handleSearchboxChange = e => {
     console.log(e.target.value);
   }
-  sortByFirstName = () =>{
+  sortByFirstName = () => {
     const newOrder = this.state.employees;
-    newOrder.sort(function(a,b) {
-      return ( (this.state.ascend) ? 
-      a.name.first.localeCompare(b.name.first)
-      : b.name.first.localeCompare(a.name.first))
+    const doesAscend = this.state.ascend;
+    newOrder.sort(function (a, b) {
+
+      return ((doesAscend) ?
+        a.name.first.localeCompare(b.name.first)
+        : b.name.first.localeCompare(a.name.first))
     });
-    this.setState({employees: newOrder})
+    this.setState(
+      {
+        employees: newOrder,
+        ascend: !doesAscend
+      }
+    )
   }
-  sortByLastName = () =>{
+  sortByLastName = () => {
     const newOrder = this.state.employees;
-    newOrder.sort(function(a,b) {
-      return ( (this.state.ascend) ? a.name.last.localeCompare(b.name.last)
-      : b.name.last.localeCompare(a.name.last))
+    const doesAscend = this.state.ascend;
+
+    newOrder.sort(function (a, b) {
+      return ((doesAscend) ? a.name.last.localeCompare(b.name.last)
+        : b.name.last.localeCompare(a.name.last))
     });
-    this.setState({employees: newOrder})
+    this.setState(
+      {
+        employees: newOrder,
+        ascend: !doesAscend
+      }
+    )
   }
-  sortByPhone = () =>{
+  sortByPhone = () => {
     const newOrder = this.state.employees;
-    newOrder.sort(function(a,b) {
-      return ( (this.state.ascend) ? a.cell.localeCompare(b.cell) :
-      b.cell.localeCompare(a.cell))
+    newOrder.sort(function (a, b) {
+      return ((this.state.ascend) ? a.cell.localeCompare(b.cell) :
+        b.cell.localeCompare(a.cell))
     });
-    this.setState({employees: newOrder})
+    this.setState({ employees: newOrder })
   }
-  sortByEmail = () =>{
+  sortByEmail = () => {
     const newOrder = this.state.employees;
-    newOrder.sort(function(a,b) {
-      return ( (this.state.ascend) ? a.email.localeCompare(b.email) :
-      b.email.localeCompare(a.email))
+    newOrder.sort(function (a, b) {
+      return ((this.state.ascend) ? a.email.localeCompare(b.email) :
+        b.email.localeCompare(a.email))
     });
-    this.setState({employees: newOrder})
+    this.setState({ employees: newOrder })
 
   }
-  sortByDOB = () =>{
+  sortByDOB = () => {
     const newOrder = this.state.employees;
-    newOrder.sort(function(a,b) {
-      return ( (this.state.ascend) ? a.dob.date.localeCompare(b.dob.date))
+    newOrder.sort(function (a, b) {
+      return ((this.state.ascend) ? a.dob.date.localeCompare(b.dob.date)
+        : b.dob.date.localeCompare(a.dob.date))
     });
-    this.setState({employees: newOrder})
+    this.setState({ employees: newOrder })
 
   }
-  render(){
-  return (
-    <div className="App">
-    <Navbar handleSearchboxChange= {this.handleSearchboxChange}/>  
-    <Table 
-    employees={this.state.employees} 
-    onFNameSort={this.sortByFirstName}
-    onLNameSort={this.sortByLastName}
-    onPhoneSort={this.sortByPhone}
-    onEmailSort={this.sortByEmail}
-    onDOBSort={this.sortByDOB}
-    />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Navbar handleSearchboxChange={this.handleSearchboxChange} />
+        <Table
+          employees={this.state.employees}
+          onFNameSort={this.sortByFirstName}
+          onLNameSort={this.sortByLastName}
+          onPhoneSort={this.sortByPhone}
+          onEmailSort={this.sortByEmail}
+          onDOBSort={this.sortByDOB}
+        />
+      </div>
+    );
   }
 }
 
